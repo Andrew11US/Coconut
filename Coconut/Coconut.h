1fe1b209b4fa3a7b36bfaf654ff46611da4e63a5
Andrii Halabuda <andrii.halabuda@gmail.com>
2022-10-23 18:53:01 -0400

    Project setup

---
+++ Coconut/Coconut.h
@@ -0,0 +1,18 @@
+//
+//  Coconut.h
+//  Coconut
+//
+//  Created by Andrew on 2022-10-23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Coconut.
+FOUNDATION_EXPORT double CoconutVersionNumber;
+
+//! Project version string for Coconut.
+FOUNDATION_EXPORT const unsigned char CoconutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Coconut/PublicHeader.h>
+
+
